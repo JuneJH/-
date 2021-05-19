16d03c80cfb09735bd0ff6871ac3237b19d2d73a
JuneJH <15310487388@163.com>
2021-05-19 21:49:23 +0800

    配置路由

--- client/.umirc.ts
+++ client/.umirc.ts
@@ -4,9 +4,6 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
   proxy: {
     "/api":{
       target:"http://127.0.0.1:8080",
